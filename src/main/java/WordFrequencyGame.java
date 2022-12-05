a7814a290b5fe780e6b789a8293939331ffd39cf
tokaho <tokaho2000@gmail.com>
2022-12-05 19:04:04 +0800

    [refactor] extract count and sort logic from getResult to getListMap

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.io.CharArrayWriter;
 
 import java.time.LocalDateTime;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class WordFrequencyGame {
@@ -17,13 +18,7 @@ public class WordFrequencyGame {
                                             .collect(Collectors.toList());
 
                 //get the map for the next step of sizing the same word
-                Map<String, List<Input>> map =getListMap(inputList);
-
-                inputList = map.entrySet().stream()
-                                .map(inputEntry -> new Input(inputEntry.getKey(),inputEntry.getValue().size()))
-                                .collect(Collectors.toList());
-
-                inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+                inputList = getListMap(inputList);
 
                 return inputList.stream()
                         .map(word -> String.format("%s %d",word.getValue(),word.getWordCount()))
@@ -36,23 +31,14 @@ public class WordFrequencyGame {
     }
 
 
-    private Map<String,List<Input>> getListMap(List<Input> inputList) {
-        Map<String, List<Input>> map = new HashMap<>();
-        for (Input input :  inputList){
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(input.getValue())){
-                ArrayList arr = new ArrayList<>();
-                arr.add(input);
-                map.put(input.getValue(), arr);
-            }
-
-            else {
-                map.get(input.getValue()).add(input);
-            }
-        }
-
-
-        return map;
+    private List<Input> getListMap(List<Input> inputList) {
+        return inputList.stream()
+                .map(input->input.getValue())
+                .collect(Collectors.groupingBy( Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .map(inputEntry -> new Input(inputEntry.getKey(),inputEntry.getValue().intValue()))
+                .sorted((w1, w2) -> w2.getWordCount() - w1.getWordCount())
+                .collect(Collectors.toList());
     }
 
 
